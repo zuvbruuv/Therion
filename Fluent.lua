a8dd22589b0aa4bae0b0611951c8723a8b6ed942
zuvbruuv <152209324+zuvbruuv@users.noreply.github.com>
2024-11-17 14:45:49 -0500

    Update Fluent.lua

--- Fluent.lua
+++ Fluent.lua
@@ -6651,7 +6651,39 @@ local ClosureBindings = {
 			HoverChange = 0.04,
 			}
 
-		end)() end
+		end)() end,
+    [55] = function()local wax,script,require=ImportGlobals(52)local ImportGlobals return (function(...)return {
+			Name = "Dark Blue",
+			Accent = Color3.fromRGB(20, 100, 180),
+
+			AcrylicMain = Color3.fromRGB(30, 30, 30),
+			AcrylicBorder = Color3.fromRGB(60, 60, 60),
+			AcrylicGradient = ColorSequence.new(Color3.fromRGB(25, 25, 25), Color3.fromRGB(15, 15, 15)),
+			AcrylicNoise = 0.94,
+
+			TitleBarLine = Color3.fromRGB(45, 50, 60),
+			Tab = Color3.fromRGB(35, 40, 45),
+
+			Element = Color3.fromRGB(30, 35, 40),
+			ElementBorder = Color3.fromRGB(45, 50, 60),
+			InElementBorder = Color3.fromRGB(40, 45, 50),
+			ElementTransparency = 0.82,
+
+			DropdownFrame = Color3.fromRGB(30, 35, 40),
+			DropdownHolder = Color3.fromRGB(30, 35, 40),
+			DropdownBorder = Color3.fromRGB(45, 50, 60),
+
+			Dialog = Color3.fromRGB(35, 35, 35),
+			DialogHolder = Color3.fromRGB(25, 25, 25),
+			DialogHolderLine = Color3.fromRGB(20, 20, 20),
+			DialogButton = Color3.fromRGB(35, 35, 35),
+			DialogButtonBorder = Color3.fromRGB(55, 55, 55),
+			DialogBorder = Color3.fromRGB(50, 50, 50),
+			DialogInput = Color3.fromRGB(45, 45, 45),
+			DialogInputLine = Color3.fromRGB(120, 120, 120),
+			}
+
+		end)() end,
 } -- [RefId] = Closure
 
 -- Holds the actual DOM data
