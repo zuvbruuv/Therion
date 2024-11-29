3d4fa43ff350ba4e569d117ae395fbbe033ed40f
zuvbruuv <152209324+zuvbruuv@users.noreply.github.com>
2024-11-29 05:55:39 -0500

    Update GameVersion.lua

--- GameVersion.lua
+++ GameVersion.lua
@@ -1,5 +1,5 @@
 return {
-    [12062942034] = "(12af09)"; -- Heroes Online World
-    [10561456271] = "(bcfa18)"; -- The Vampire Legacies 2 (Menu)
-    [10561483644] = "(bcfa18)"; -- The Vampire Legacies 2 (Mystic Falls)
+    [12062942034] = '(12af09)', -- Heroes Online World
+    [10561456271] = '(bcfa18)', -- The Vampire Legacies 2 (Menu)
+    [10561483644] = '(bcfa18)', -- The Vampire Legacies 2 (Mystic Falls)
 }
