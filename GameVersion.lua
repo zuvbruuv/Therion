97bca2330a22ddc1a346cf21be47a0e923b0d2cf
zuvbruuv <152209324+zuvbruuv@users.noreply.github.com>
2024-12-18 17:42:36 -0500

    Update GameVersion.lua

--- GameVersion.lua
+++ GameVersion.lua
@@ -1,9 +1,6 @@
 return {
-    -- // Heroes Online World
-    ['HeroesOnlineWorld'] = '(12af09)',
-
     -- // The Vampire Legacies 2
-    [3837114739] = '(bcfa18)',
+    [3837114739] = '(a065fa)',
 
     -- // Arcane Odyssey
     [1180269832] = '(4cf7a0)'
