b2048584f1551cbdab15f58805f18ca2966e7e10
zuvbruuv <152209324+zuvbruuv@users.noreply.github.com>
2024-12-21 23:46:52 -0500

    Update GameVersion.lua

--- GameVersion.lua
+++ GameVersion.lua
@@ -2,6 +2,6 @@ return {
     -- // The Vampire Legacies 2
     [3837114739] = '(a065fa)',
 
-    -- // Arcane Odyssey
-    [1180269832] = '(4cf7a0)'
+    -- // Attack On Titan Revolution
+    [4658598196] = '(a065fa)'
 }
