389efb15818510a4a91c0aa2792b3a8e98cb68a3
zuvbruuv <152209324+zuvbruuv@users.noreply.github.com>
2025-01-20 09:51:32 -0500

    Update Version.lua

--- Version.lua
+++ Version.lua
@@ -2,12 +2,10 @@ return {
     -- The Vampire Legacies 2
     [3837114739] = {
         GameVersion = '(1085fc)',
-        ScriptVersion = 'v1.0.0'
     },
 
     -- Attack On Titan Revolution
     [4658598196] = {
-        GameVersion = '(a065fa)',
-        ScriptVersion = 'v1.0.0'
+        GameVersion = '(1085fc',
     }
 }
