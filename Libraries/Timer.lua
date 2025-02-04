32857de87f93a867460e3e50fc116195cb3800bb
zuvbruv <152209324+zuvbruuv@users.noreply.github.com>
2025-02-04 11:47:24 -0500

    Update Timer.lua

--- Libraries/Timer.lua
+++ Libraries/Timer.lua
@@ -1,199 +1,230 @@
+-- Timer
+-- Stephen Leitnick
+-- July 28, 2021
+
+--[=[
+	@within Timer
+	@type CallbackFn () -> ()
+	Callback function.
+]=]
+type CallbackFn = () -> nil
+
+--[=[
+	@within Timer
+	@type TimeFn () -> number
+	Time function.
+]=]
+type TimeFn = () -> number
+
+local Signal = require(script.Parent.Signal)
+
+local RunService = game:GetService("RunService")
+
+--[=[
+	@class Timer
+
+	The Timer class allows for code to run periodically at specified intervals.
+
+	```lua
+	local timer = Timer.new(2)
+	timer.Tick:Connect(function()
+		print("Tock")
+	end)
+	timer:Start()
+	```
+]=]
 local Timer = {}
-local TimerEnum = setmetatable({
-	Running = 'Running',
-	Paused = 'Paused',
-	Finished = 'Finished',
-	Stopped = 'Stopped',
-	Dead = 'Dead',
-	Died = 'Died'
-}, {
-	__index = function(_, key)
-		return error(string.format('%s is not a valid member of %s', key, 'TimerEnum'), 2)
-	end,
-	__newindex = function()
-		return error('attempt to modify a readonly table', 2)
-	end,
-	__metatable = false,
-	__tostring = 'TimerEnum'
-})
+Timer.__index = Timer
 
-local TimerArrays = {}
-local RunService = game:GetService('RunService')
+--[=[
+	@within Timer
+	@prop Interval number
+	Interval at which the `Tick` event fires.
+]=]
+--[=[
+	@within Timer
+	@prop UpdateSignal RBXScriptSignal | Signal
+	The signal which updates the timer internally.
+]=]
+--[=[
+	@within Timer
+	@prop TimeFunction TimeFn
+	The function which gets the current time.
+]=]
+--[=[
+	@within Timer
+	@prop AllowDrift boolean
+	Flag which indicates if the timer is allowed to drift. This
+	is set to `true` by default. This flag must be set before
+	calling `Start` or `StartNow`. This flag should only be set
+	to `false` if it is necessary for drift to be eliminated.
+]=]
+--[=[
+	@within Timer
+	@prop Tick RBXScriptSignal | Signal
+	The event which is fired every time the timer hits its interval.
+]=]
 
-local TimerClass = {}
-TimerClass.__index = TimerClass
+--[=[
+	@return Timer
+	
+	Creates a new timer.
+]=]
+function Timer.new(interval: number)
+	assert(type(interval) == "number", "Argument #1 to Timer.new must be a number; got " .. type(interval))
+	assert(interval >= 0, "Argument #1 to Timer.new must be greater or equal to 0; got " .. tostring(interval))
+	local self = setmetatable({}, Timer)
+	self._runHandle = nil
+	self.Interval = interval
+	self.UpdateSignal = RunService.Heartbeat
+	self.TimeFunction = time
+	self.AllowDrift = true
+	self.Tick = Signal.new()
+	return self
+end
 
-local function StateCallback(TimerObject, State, ...)
-	if TimerObject.BotherChecking then
-		for _, Function in next, TimerObject.CallbackStates[State] do
-			coroutine.wrap(Function)(State, ...)
+--[=[
+	@return RBXScriptConnection
+
+	Creates a simplified timer which just fires off a callback function at the given interval.
+
+	```lua
+	-- Basic:
+	Timer.Simple(1, function()
+		print("Tick")
+	end)
+
+	-- Using other arguments:
+	Timer.Simple(1, function()
+		print("Tick")
+	end, true, RunService.Heartbeat, os.clock)
+	```
+]=]
+function Timer.Simple(
+	interval: number,
+	callback: CallbackFn,
+	startNow: boolean?,
+	updateSignal: RBXScriptSignal?,
+	timeFn: TimeFn?
+)
+	local update = updateSignal or RunService.Heartbeat
+	local t = timeFn or time
+	local nextTick = t() + interval
+	if startNow then
+		task.defer(callback)
+	end
+	return update:Connect(function()
+		local now = t()
+		if now >= nextTick then
+			nextTick = now + interval
+			task.defer(callback)
 		end
-	end
+	end)
 end
 
-local function EndTimer(TimerObject, State, ...)
-	if TimerObject.Callback then
-		coroutine.wrap(TimerObject.Callback)(State)
-	end
+--[=[
+	Returns `true` if the given object is a Timer.
+]=]
+function Timer.Is(obj: any): boolean
+	return type(obj) == "table" and getmetatable(obj) == Timer
+end
 
-	if TimerObject.BotherChecking then
-		if State == TimerEnum.Stopped or State == 'Finished' then
-			for _, StateName in next, {TimerEnum.Dead, TimerEnum.Died} do
-				for _, Function in next, TimerObject.CallbackStates[StateName] do
-					coroutine.wrap(Function)(StateName, ...)
-				end
-			end
+function Timer:_startTimer()
+	local t = self.TimeFunction
+	local nextTick = t() + self.Interval
+	self._runHandle = self.UpdateSignal:Connect(function()
+		local now = t()
+		if now >= nextTick then
+			nextTick = now + self.Interval
+			self.Tick:Fire()
 		end
+	end)
+end
 
-		for _, Function in next, TimerObject.CallbackStates[State] do
-			coroutine.wrap(Function)(State, ...)
+function Timer:_startTimerNoDrift()
+	assert(self.Interval > 0, "Interval must be greater than 0 when AllowDrift is set to false")
+	local t = self.TimeFunction
+	local n = 1
+	local start = t()
+	local nextTick = start + self.Interval
+	self._runHandle = self.UpdateSignal:Connect(function()
+		local now = t()
+		while now >= nextTick do
+			n += 1
+			nextTick = start + (self.Interval * n)
+			self.Tick:Fire()
 		end
-	end
-	TimerObject.State = TimerEnum.Dead
+	end)
 end
 
---// Thank you CntKillMe
-local function FindBestSpot(CompletionTime)
-	local LeftIdx = 1
-	local RightIdx = #TimerArrays
+--[=[
+	Starts the timer. Will do nothing if the timer is already running.
 
-	if RightIdx == 0 then
-		return 1
+	```lua
+	timer:Start()
+	```
+]=]
+function Timer:Start()
+	if self._runHandle then
+		return
 	end
-	while LeftIdx <= RightIdx do
-		local CenterIdx = math.floor((LeftIdx + RightIdx) / 2)
-		local CenterTimer = TimerArrays[CenterIdx]
-
-		if CenterTimer == nil then
-			return RightIdx
-		end
-		if CompletionTime > CenterTimer:GetRemaining() then
-			RightIdx = CenterIdx - 1
-		elseif CompletionTime < CenterTimer:GetRemaining() then
-			LeftIdx = CenterIdx + 1
-		else
-			LeftIdx = CenterIdx
-			break
-		end
-	end
-
-	return LeftIdx
-end
-local function AddToArray(TimerObj)
-	table.insert(TimerArrays, FindBestSpot(TimerObj:GetRemaining()), TimerObj)
-end
-local function RemoveFromArray(TimerObj)
-	local Index = table.find(TimerArrays, TimerObj)
-	if Index then
-		table.remove(TimerArrays, Index)
+	if self.AllowDrift then
+		self:_startTimer()
+	else
+		self:_startTimerNoDrift()
 	end
 end
 
-function TimerClass:GetLength()
-	return self.Length
-end
-function TimerClass:GetRemaining()
-	local Time = self.FrozenRemaining or self.Length - (time() - self.Started)
-	return Time > 0 and Time or 0
-end
-function TimerClass:GetState()
-	return self.State
-end
+--[=[
+	Starts the timer and fires off the Tick event immediately. Will do
+	nothing if the timer is already running.
 
-for _, Enum in next, TimerEnum do
-	TimerClass['Is' .. Enum] = function(self)
-		assert(self, 'Expected \':\' not \'.\' calling member function Is' .. Enum)
-		return self.State == Enum
+	```lua
+	timer:StartNow()
+	```
+]=]
+function Timer:StartNow()
+	if self._runHandle then
+		return
 	end
+	self.Tick:Fire()
+	self:Start()
 end
 
-function TimerClass:Start()
-	self.Started = time()
-	AddToArray(self)
-	self.State = TimerEnum.Running
-	StateCallback(self, self.State, self:GetRemaining())
-end
-function TimerClass:Wait()
-	table.insert(self.YieldedThreads, coroutine.running())
-	return coroutine.yield()
-end
-function TimerClass:Pause()
-	RemoveFromArray(self)
-	self.State = TimerEnum.Paused
-	self.FrozenRemaining = self:GetRemaining()
-	StateCallback(self, self.State, time() - self.Started)
-end
-function TimerClass:Resume()
-	assert(self.State == TimerEnum.Paused, 'Attempt to resume a non-frozen timer (State: ' .. self.State .. ')')
-	AddToArray(self)
-	self.State = TimerEnum.Running
-	self.FrozenRemaining = nil
-	StateCallback(self, self.State, time() - self.Started)
-end
-function TimerClass:Stop()
-	assert(self.State ~= TimerEnum.Stopped, 'Attempt to stop a dead timer (State: ' .. self.State .. ')')
-	self.State = TimerEnum.Stopped
-	RemoveFromArray(self)
-	EndTimer(self, self.State, time() - self.Started)
-end
-function TimerClass:IncrementTime(Delta)
-	self.Length += Delta
-end
+--[=[
+	Stops the timer. Will do nothing if the timer is already stopped.
 
--- Aliases
-TimerClass.Yield = TimerClass.Wait
-TimerClass.Yield = TimerClass.Wait
-TimerClass.Play = TimerClass.Start
-TimerClass.Cancel = TimerClass.Stop
-TimerClass.Kill = TimerClass.Stop
-
-
-for State in next, TimerEnum do
-	TimerClass['On' .. State] = function(self, f)
-		assert(type(f) == 'function', 'Invalid argument #1 to On' .. State .. ' (function expected, got ' .. typeof(f) .. ')')
-		self.BotherChecking = true
-		table.insert(self.CallbackStates[State], f)
+	```lua
+	timer:Stop()
+	```
+]=]
+function Timer:Stop()
+	if not self._runHandle then
+		return
 	end
+	self._runHandle:Disconnect()
+	self._runHandle = nil
 end
 
-Timer.new = function(Wait)
-	local CallbackStates = {}
-	for State in next, TimerEnum do
-		CallbackStates[State] = {}
-	end
+--[=[
+	Returns `true` if the timer is currently running.
 
-	return setmetatable({
-		Length = Wait,
-		Started = time(),
-		State = TimerEnum.Paused,
-		YieldedThreads = {},
-		CallbackStates = CallbackStates
-	}, TimerClass)
+	```lua
+	if timer:IsRunning() then
+		-- Do something
+	end
+	```
+]=]
+function Timer:IsRunning(): boolean
+	return self._runHandle ~= nil
 end
 
-RunService.Stepped:Connect(function()
-	local time = time()
-	local DyingObj = TimerArrays[#TimerArrays]
-
-	while DyingObj do
-		if DyingObj:GetRemaining() <= 0 then
-			local Time = time - DyingObj.Started
-
-			TimerArrays[#TimerArrays] = nil
-			EndTimer(DyingObj, TimerEnum.Finished, Time)
-
-			for _, Thread in next, DyingObj.YieldedThreads do
-				coroutine.resume(Thread, TimerEnum.Finished, Time)
-			end
-			DyingObj = TimerArrays[#TimerArrays]
-			continue
-		end
-		DyingObj = nil
-	end
-end)
-Timer.Enums = TimerEnum
-function Timer:GetEnums()
-	return TimerEnum
+--[=[
+	Destroys the timer. This will also stop the timer.
+]=]
+function Timer:Destroy()
+	self.Tick:Destroy()
+	self:Stop()
 end
 
 return Timer
