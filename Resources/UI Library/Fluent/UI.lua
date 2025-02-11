0fc9a5d26cfc4a853d77e342f2b380bc2928f03c
zuvbruv <152209324+zuvbruuv@users.noreply.github.com>
2025-02-11 10:40:21 -0500

    Update UI.lua

--- Resources/UI Library/Fluent/UI.lua
+++ Resources/UI Library/Fluent/UI.lua
@@ -1572,7 +1572,7 @@ local ClosureBindings = {
 
 				local Logo = New("ImageLabel", {
 					Name = "Icon",
-					Image = "rbxassetid://135567531083117",
+					Image = "rbxassetid://77910598271973",
 					ImageColor3 = Color3.fromRGB(240, 240, 240),
 					AnchorPoint = Vector2.new(0.5, 0.5),
 					BackgroundColor3 = Color3.fromRGB(255, 255, 255),
